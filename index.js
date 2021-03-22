8ff4c754becbe4c53a4b3fd3d659f961d77e0759
Anahi Quintero <anitaquintero99@gmail.com>
2021-03-21 20:14:28 -0600

    Prompt for user input

--- index.js
+++ index.js
@@ -2,9 +2,32 @@
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+const inquirer = require("inquirer");
 
-clear();
-const divider = Array(40).fill("*").join(" ");
-const title = figlet.textSync("H A N G M A N");
-const titleColor = chalk.yellow(`${divider}\n${title}\n${divider}`);
-console.log(titleColor);
+function logMultiplier(number, symbolFill = "*", symbolJoin = " ") {
+  return Array(number).fill(symbolFill).join(symbolJoin);
+}
+
+async function main() {
+  clear();
+  const divider = logMultiplier(40);
+  const title = figlet.textSync("H A N G M A N");
+  const titleColor = chalk.yellow(`${divider}\n${title}\n${divider}`);
+  console.log(titleColor);
+
+  const SECRET_WORD = "curtain";
+  const SECRET_WORD_LENGHT = SECRET_WORD.length;
+
+  const wordSpaces = logMultiplier(SECRET_WORD_LENGHT, "_");
+  console.log(wordSpaces);
+
+  const answer = await inquirer.prompt([
+    {
+      name: "letter",
+      message: "Ingresa una letra:",
+    },
+  ]);
+  console.log(answer);
+}
+
+main();
