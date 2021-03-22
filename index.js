1a3cb81040c17100ed7c0488e5f0f1527092d36a
Anahi Quintero <anitaquintero99@gmail.com>
2021-03-21 19:34:43 -0600

    Base files and dependencies

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Holi");
+//Libraries
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+
+clear();
+const divider = Array(40).fill("*").join(" ");
+const title = figlet.textSync("H A N G M A N");
+const titleColor = chalk.yellow(`${divider}\n${title}\n${divider}`);
+console.log(titleColor);
